4c9b0d3a9420c321940a100ef4079002f9b57ad6
changkyu <changkyu@bejewel.kr>
2024-12-22 19:09:47 +0900

    이미지 검색 API 생성 중

---
+++ Projects/Core/ImageSearchAPICore/Tests/ImageSearchAPICoreTest.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+final class ImageSearchAPICoreTests: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() {
+        XCTAssertEqual(1, 1)
+    }
+}
