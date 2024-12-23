e4b81dfbc7b172fe0e9cdd2d966939e8e3136892
talq44 <talq44@gmail.com>
2024-12-23 15:56:19 +0900

    [NO-ISSUE] 도메인 추가

--- Tuist/ProjectDescriptionHelpers/Modules/DomainModule.swift
+++ Tuist/ProjectDescriptionHelpers/Modules/DomainModule.swift
@@ -8,11 +8,13 @@
 import ProjectDescription
 
 public enum DomainModule: String, CaseIterable {
+	case ViewItemDomain
     case BookmarkListDomain
 	case BookmarkUpdateDomain
     case DomainModelsDomain
     case ItemListInteractionDomain
 	case SearchListDomain
+    case ViewItemDomain
     
     public var name: String {
         self.rawValue
