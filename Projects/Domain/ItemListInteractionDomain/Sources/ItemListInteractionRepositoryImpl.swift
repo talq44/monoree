6cc748191728406fdfc8c26ad697bafb3db9cf6f
talq44 <talq44@gmail.com>
2024-12-23 14:57:48 +0900

    [NO-ISSUE] 구현부 선언

--- Projects/Domain/ItemListInteractionDomain/Sources/ItemListInteractionRepositoryImpl.swift
+++ Projects/Domain/ItemListInteractionDomain/Sources/ItemListInteractionRepositoryImpl.swift
@@ -1,8 +1,38 @@
-//
-//  ItemListInteractionRepositoryImpl.swift
-//  ItemListInteractionDomainInterface
-//
-//  Created by 박창규 on 12/23/24.
-//
-
 import Foundation
+
+import ItemListInteractionDomainInterface
+
+final class ItemListInteractionRepositoryImplementation: ItemListIntercationRepository {
+    
+    func sendViewItemList(
+        items: [any ItemListInteractionDomainInterface.ItemListItem],
+        itemList: ItemListInteractionDomainInterface.ItemList
+    ) {
+        self.fakeSend(
+            items: items,
+            itemListId: itemList.itemListId,
+            itemListName: itemList.itemListId
+        )
+    }
+    
+    func sendSelectItem(
+        items: [any ItemListInteractionDomainInterface.ItemListItem],
+        itemList: ItemListInteractionDomainInterface.ItemList
+    ) {
+        self.fakeSend(
+            items: items,
+            itemListId: itemList.itemListId,
+            itemListName: itemList.itemListId
+        )
+    }
+    
+    private func fakeSend(
+        items: [any ItemListItem],
+        itemListId: String?,
+        itemListName: String?
+    ) {
+        print("items \(items)")
+        print("itemListId \(itemListId ?? "")")
+        print("itemListName \(itemListName ?? "")")
+    }
+}
--- Projects/Domain/ItemListInteractionDomain/Sources/ItemListInteractionUseCaseImpl.swift
+++ Projects/Domain/ItemListInteractionDomain/Sources/ItemListInteractionUseCaseImpl.swift
@@ -1,8 +1,27 @@
-//
-//  ItemListInteractionUseCaseImpl.swift
-//  ItemListInteractionDomainInterface
-//
-//  Created by 박창규 on 12/23/24.
-//
-
 import Foundation
+
+import ItemListInteractionDomainInterface
+
+final class ItemListInteractionUseCaseImpl: ItemListIntercationUseCase {
+    private let repository: ItemListIntercationRepository
+    
+    public init(repository: ItemListIntercationRepository) {
+        self.repository = repository
+    }
+    
+    func send(input: any ItemListIntercationInput) {
+        switch input.sendType {
+        case .selectItem:
+            self.repository.sendSelectItem(
+                items: input.items,
+                itemList: input.itemList
+            )
+            
+        case .viewItemList:
+            self.repository.sendViewItemList(
+                items: input.items,
+                itemList: input.itemList
+            )
+        }
+    }
+}
