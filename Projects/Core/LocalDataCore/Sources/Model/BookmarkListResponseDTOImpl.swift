a27e1cdba9343d0bb6ef7fe33bb69f5249207d53
talq44 <talq44@gmail.com>
2025-03-18 21:52:52 +0900

    [#13] 사용하지 않는 realm 삭제

--- Projects/Core/LocalDataCore/Sources/Model/BookmarkListResponseDTOImpl.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-import LocalDataCoreInterface
-
-import RealmSwift
-
-struct BookmarkListResponseDTOImpl: BookmarkListResponseDTO {
-    let items: [any LocalDataCoreInterface.BookmarkItemDTO]
-}
