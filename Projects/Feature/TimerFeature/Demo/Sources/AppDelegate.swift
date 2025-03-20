a3baa1d1c69852eed45b565176bc24259f204519
talq44 <talq44@gmail.com>
2025-03-20 13:24:15 +0900

    [#13] 타이머 노출을 위한 연결

--- Projects/Feature/TimerFeature/Demo/Sources/AppDelegate.swift
+++ Projects/Feature/TimerFeature/Demo/Sources/AppDelegate.swift
@@ -1,19 +1,34 @@
-import UIKit
+import SwiftUI
+
+import ComposableArchitecture
+
+import TimerFeature
 
 @main
-final class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = UIViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-
-        return true
+struct DemoView: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            TimerContainer().build().view
+//            ZStack {
+//                Text("Hi")
+//            }
+        }
     }
 }
+//final class AppDelegate: UIResponder, UIApplicationDelegate {
+//    var window: UIWindow?
+//
+//    func application(
+//        _ application: UIApplication,
+//        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+//    ) -> Bool {
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        let viewController = UIViewController()
+//        let navigationController = UINavigationController(rootViewController: viewController)
+//        window?.rootViewController = navigationController
+//        window?.makeKeyAndVisible()
+//
+//        return true
+//    }
+//}
