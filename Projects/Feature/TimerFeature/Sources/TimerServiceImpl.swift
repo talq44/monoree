3a7160599a0237211019a22862f7fecf4ab0b7b1
talq44 <talq44@gmail.com>
2025-03-24 10:20:09 +0900

    [#13] 완료시점 전달

--- Projects/Feature/TimerFeature/Sources/TimerServiceImpl.swift
+++ Projects/Feature/TimerFeature/Sources/TimerServiceImpl.swift
@@ -1,15 +1,14 @@
-import Foundation
 import Combine
-import SwiftUI
-
-import TimerFeatureInterface
-
 import ComposableArchitecture
+import Foundation
+import SwiftUI
+import TimerFeatureInterface
 
 // MARK: - TimerFeature.Service (TMA 구조 적용)
 final public class TimerServiceImpl: TimerService {
     private let store: StoreOf<TimerReducer>
     private var finishedSubject = PassthroughSubject<Bool, Never>()
+    private var cancellables = Set<AnyCancellable>()
     
     public init(initialTime: TimerFeatureInterface.TimeType = .seconds_3) {
         self.store = .init(
@@ -18,6 +17,13 @@ final public class TimerServiceImpl: TimerService {
             ),
             reducer: { TimerReducer() }
         )
+        
+        // isComplete 상태 변화 감지
+        store.publisher.isComplete
+            .sink { [weak self] isComplete in
+                self?.finishedSubject.send(isComplete)
+            }
+            .store(in: &cancellables)
     }
     
     public func setTime(_ time: TimerFeatureInterface.TimeType) {
@@ -26,7 +32,7 @@ final public class TimerServiceImpl: TimerService {
     
     public func start() -> AnyPublisher<Bool, Never> {
         store.send(.start)
-        return finishedSubject.eraseToAnyPublisher()
+        return self.finishedSubject.eraseToAnyPublisher()
     }
     
     public func stop() {
