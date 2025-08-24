03449d573f588782e8788bdfc8c5ac49a5c8a059
changkyu <43027098+talq44@users.noreply.github.com>
2025-08-24 22:44:49 +0900

    Update Projects/Core/RemoteConfigCore/Project.swift
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Projects/Core/RemoteConfigCore/Project.swift
+++ Projects/Core/RemoteConfigCore/Project.swift
@@ -5,29 +5,32 @@ let project = Project.module(
     name: CoreModule.RemoteConfigCore.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			core: .RemoteConfigCore,
-			dependencies: []
-		),
-		.implementation(
-			core: .RemoteConfigCore,
-			dependencies: [
+        .interface(
+            core: .RemoteConfigCore,
+            dependencies: []
+        ),
+        .implementation(
+            core: .RemoteConfigCore,
+            dependencies: [
                 .shared(target: .FirebaseSPMShared),
-				.core(target: .RemoteConfigCore, type: .interface),
-			]
-		),
-		.testing(
-			core: .RemoteConfigCore,
-			dependencies: [
-				.core(target: .RemoteConfigCore, type: .interface),
-			]
-		),
-		.tests(
-			core: .RemoteConfigCore,
-			dependencies: [
-				.core(target: .RemoteConfigCore, type: .implementation),
-				.core(target: .RemoteConfigCore, type: .testing),
-			]
-		),
-]
+                .core(target: .RemoteConfigCore, type: .interface),
+            ],
+            resources: [
+                .file(path: "Projects/Core/RemoteConfigCore/Sources/remote_config_defaults.plist")
+            ]
+        ),
+        .testing(
+            core: .RemoteConfigCore,
+            dependencies: [
+                .core(target: .RemoteConfigCore, type: .interface),
+            ]
+        ),
+        .tests(
+            core: .RemoteConfigCore,
+            dependencies: [
+                .core(target: .RemoteConfigCore, type: .implementation),
+                .core(target: .RemoteConfigCore, type: .testing),
+            ]
+        ),
+    ]
 )
