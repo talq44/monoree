7a6ba700812d9410d5b3af676bdd601adea3f4d8
talq44 <talq44@gmail.com>
2025-08-24 21:00:46 +0900

    [#22] 5.10.0으로 환경 다운로드

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,35 +1,35 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 import PackageDescription
 
 #if TUIST
-import ProjectDescription
+  import ProjectDescription
 
-let packageSettings = PackageSettings(
+  let packageSettings = PackageSettings(
     // Customize the product types for specific package product
     // Default is .staticFramework
     // productTypes: ["Alamofire": .framework,]
     productTypes: [:]
-)
+  )
 #endif
 
 let package = Package(
-    name: "Monory",
-    platforms: [
-        .iOS(.v16)
-    ],
-    dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.1")),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "8.1.1")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.1")),
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.7.1")),
-        .package(url: "https://github.com/ReactorKit/ReactorKit.git", .upToNextMajor(from: "3.2.0")),
-        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1"),
-        .package(url: "https://github.com/vadymmarkov/Fakery", from: "5.0.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "12.1.0")),
-        .package(
-            url: "https://github.com/pointfreeco/swift-composable-architecture",
-            .upToNextMinor(from: "1.18.0")
-        ),
-    ]
+  name: "Monory",
+  platforms: [
+    .iOS(.v16)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.1")),
+    .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "8.1.1")),
+    .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.1")),
+    .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3")),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.7.1")),
+    .package(url: "https://github.com/ReactorKit/ReactorKit.git", .upToNextMajor(from: "3.2.0")),
+    .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1"),
+    .package(url: "https://github.com/vadymmarkov/Fakery", from: "5.0.0"),
+    .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "11.0.0")),
+    .package(
+      url: "https://github.com/pointfreeco/swift-composable-architecture",
+      .upToNextMinor(from: "1.18.0")
+    ),
+  ]
 )
