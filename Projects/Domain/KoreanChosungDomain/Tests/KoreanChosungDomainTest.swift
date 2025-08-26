084e4369bbfbc450a36bface1c90b040881a55dc
talq44 <talq44@gmail.com>
2025-08-26 23:05:39 +0900

    [#32] 테스트 코드 추가

--- Projects/Domain/KoreanChosungDomain/Tests/KoreanChosungDomainTest.swift
+++ Projects/Domain/KoreanChosungDomain/Tests/KoreanChosungDomainTest.swift
@@ -12,7 +12,7 @@ final class KoreanChosungDomainTests: XCTestCase {
         sut = nil
     }
     
-    func test_given_속담_when_usecase_then_정답() throws {
+    func test_given_초성_속담_when_usecase_then_정답() throws {
         // given
         let inputs = [
             "가는 말이 고와야 오는 말이 곱다",
@@ -51,7 +51,51 @@ final class KoreanChosungDomainTests: XCTestCase {
         for (index, result) in results.enumerated() {
             XCTAssertTrue(
                 result.question == expected[index],
-                "\(result.answer) 실패 자음만 분리된 글자와 동일해야 한다."
+                "\(result.answer) 자음만 분리된 글자와 동일해야 한다."
+            )
+        }
+    }
+    
+    func test_given_중성_속담_when_usecase_then_정답() throws {
+        // given
+        let inputs = [
+            "가는 말이 고와야 오는 말이 곱다",
+            "고래 싸움에 새우 등 터진다",
+            "소 잃고 외양간 고친다",
+            "호랑이 굴에 가야 호랑이를 잡는다",
+            "백지장도 맞들면 낫다",
+            "우물 안 개구리",
+            "원숭이도 나무에서 떨어진다",
+            "까마귀 날자 배 떨어진다",
+            "티끌 모아 태산",
+            "식은 죽 먹기"
+        ]
+
+        let expected = [
+            "ㅏㅡ ㅏㅣ ㅗㅘㅑ ㅗㅡ ㅏㅣ ㅗㅏ",
+            "ㅗㅐ ㅏㅜㅔ ㅐㅜ ㅡ ㅓㅣㅏ",
+            "ㅗ ㅣㅗ ㅚㅑㅏ ㅗㅣㅏ",
+            "ㅗㅏㅣ ㅜㅔ ㅏㅑ ㅗㅏㅣㅡ ㅏㅡㅏ",
+            "ㅐㅣㅏㅗ ㅏㅡㅕ ㅏㅏ",
+            "ㅜㅜ ㅏ ㅐㅜㅣ",
+            "ㅝㅜㅣㅗ ㅏㅜㅔㅓ ㅓㅓㅣㅏ",
+            "ㅏㅏㅟ ㅏㅏ ㅐ ㅓㅓㅣㅏ",
+            "ㅣㅡ ㅗㅏ ㅐㅏ",
+            "ㅣㅡ ㅜ ㅓㅣ"
+        ]
+        
+        // when
+        let results = sut.getChosung(type: .jungsung, inputs: inputs.map({ input in
+            return .init(id: input, origin: input)
+        }))
+        
+        print(results)
+        
+        // then
+        for (index, result) in results.enumerated() {
+            XCTAssertTrue(
+                result.question == expected[index],
+                "\(result.answer) 모음만 분리된 글자와 동일해야 한다."
             )
         }
     }
