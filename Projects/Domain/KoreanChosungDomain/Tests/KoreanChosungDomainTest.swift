90e2aa3dee8432cc68a132b7fa6a27bf1095d61f
talq44 <talq44@gmail.com>
2025-08-26 23:46:21 +0900

    [#34] 테스트 코드 추가

--- Projects/Domain/KoreanChosungDomain/Tests/KoreanChosungDomainTest.swift
+++ Projects/Domain/KoreanChosungDomain/Tests/KoreanChosungDomainTest.swift
@@ -99,4 +99,94 @@ final class KoreanChosungDomainTests: XCTestCase {
             )
         }
     }
+    
+    func test_given_앞글자_다양하게_when_usecase_then_정답() throws {
+        // given
+        let inputs = [
+            "까마귀 날자 배 떨어진다",
+            "식은 죽 먹기",
+            "고진감래",
+            "호랑이 굴에 가야 호랑이를 잡는다",
+            "백지장도 맞들면 낫다",
+            "우물 안 개구리",
+            "원숭이도 나무에서 떨어진다",
+            "티끌 모아 태산",
+            "가는 말이 고와야 오는 말이 곱다",
+            "하늘의 별 따기"
+        ]
+
+        // 앞글자 기준 (makeHalfMaskedHint)
+        let expected = [
+            "까마귀 날자 O OOOO",
+            "식은 죽 OO",
+            "고진OO",
+            "호랑이 굴에 가야 OOOO OOO",
+            "백지장도 맞들면 OO",
+            "우물 안 OOO",
+            "원숭이도 나무에서 OOOO",
+            "티끌 모아 OO",
+            "가는 말이 고와야 OO OO OO",
+            "하늘의 별 OO"
+        ]
+        
+        // when
+        let results = sut.getChosung(type: .frontHalf, inputs: inputs.map({ input in
+            return .init(id: input, origin: input)
+        }))
+        
+        print(results)
+        
+        // then
+        for (index, result) in results.enumerated() {
+            XCTAssertTrue(
+                result.question == expected[index],
+                "\(result.answer) 앞글자만 노출되어야 한다."
+            )
+        }
+    }
+    
+    func test_given_뒷글자_다양하게_when_usecase_then_정답() throws {
+        // given
+        let inputs = [
+            "까마귀 날자 배 떨어진다",
+            "식은 죽 먹기",
+            "고진감래",
+            "호랑이 굴에 가야 호랑이를 잡는다",
+            "백지장도 맞들면 낫다",
+            "우물 안 개구리",
+            "원숭이도 나무에서 떨어진다",
+            "티끌 모아 태산",
+            "가는 말이 고와야 오는 말이 곱다",
+            "하늘의 별 따기"
+        ]
+
+        // 뒷글자 기준 (makeHalfMaskedHintFromEnd)
+        let expected = [
+            "OOO OO 배 떨어진다",
+            "OO 죽 먹기",
+            "OO감래",
+            "OOO OO 가야 호랑이를 잡는다",
+            "OOOO 맞들면 낫다",
+            "OO 안 개구리",
+            "OOOO 나무에서 떨어진다",
+            "OO 모아 태산",
+            "OO OO OOO 오는 말이 곱다",
+            "OOO 별 따기"
+        ]
+        
+        // when
+        let results = sut.getChosung(type: .backHalf, inputs: inputs.map({ input in
+            return .init(id: input, origin: input)
+        }))
+        
+        print(results)
+        
+        // then
+        for (index, result) in results.enumerated() {
+            XCTAssertTrue(
+                result.question == expected[index],
+                "\(result.answer) 뒷글자만 노출되어야 한다."
+            )
+        }
+    }
 }
