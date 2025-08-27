6ee4eac1a9e98c58c6d754e4f6d335ad54325982
talq44 <talq44@gmail.com>
2025-08-27 21:28:42 +0900

    [#40] version check 추가

--- Tuist/ProjectDescriptionHelpers/Modules/DomainModule.swift
+++ Tuist/ProjectDescriptionHelpers/Modules/DomainModule.swift
@@ -1,6 +1,7 @@
 import ProjectDescription
 
 public enum DomainModule: String, CaseIterable {
+	case VersionCheckDomain
 	case KoreanChosungDomain
     case BookmarkListDomain
 	case BookmarkUpdateDomain
