c0c35e7530c1146c37b9cf5340a377b6b7e7e084
talq44 <talq44@gmail.com>
2025-08-27 21:58:13 +0900

    [#40] Testing 영역 선언

--- Projects/Domain/VersionCheckDomain/Testing/MockVersionConfigManager.swift
+++ Projects/Domain/VersionCheckDomain/Testing/MockVersionConfigManager.swift
@@ -1,2 +1,38 @@
 import VersionCheckDomainInterface
 import RemoteConfigCoreInterface
+
+enum VersionCheckTestCase {
+    case error
+    case normal(
+        appStoreUrl: String,
+        minVersion: String,
+        maxVersion: String
+    )
+}
+
+final class MockVersionConfigManager: VersionConfigManager {
+    private var testCase: VersionCheckTestCase = .error
+    
+    func setup(testCase: VersionCheckTestCase) {
+        self.testCase = testCase
+    }
+    
+    func fetchVersion() throws -> any RemoteConfigCoreInterface.VersionDTO {
+        switch testCase {
+        case .error:
+            throw RemoteConfigError.unknown
+        case .normal(let appStoreUrl, let minVersion, let maxVersion):
+            return MockVersionDTO(
+                appStoreUrl: appStoreUrl,
+                minVersion: minVersion,
+                maxVersion: maxVersion
+            )
+        }
+    }
+}
+
+struct MockVersionDTO: VersionDTO {
+    let appStoreUrl: String
+    let minVersion: String
+    let maxVersion: String
+}
