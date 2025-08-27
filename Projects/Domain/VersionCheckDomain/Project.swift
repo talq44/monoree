dd18cbf52c4b29ea6ccb330807f34cbac92e55fb
talq44 <talq44@gmail.com>
2025-08-27 21:30:50 +0900

    [#40] 의존성 추가

---
+++ Projects/Domain/VersionCheckDomain/Project.swift
@@ -0,0 +1,35 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: DomainModule.VersionCheckDomain.name,
+    settings: .Module.default,
+    targets: [
+        .interface(
+            domain: .VersionCheckDomain,
+            dependencies: [
+                .core(target: .RemoteConfigCore, type: .interface),
+            ]
+        ),
+        .implementation(
+            domain: .VersionCheckDomain,
+            dependencies: [
+                .core(target: .RemoteConfigCore, type: .interface),
+                .domain(target: .VersionCheckDomain, type: .interface),
+            ]
+        ),
+        .testing(
+            domain: .VersionCheckDomain,
+            dependencies: [
+                .domain(target: .VersionCheckDomain, type: .interface),
+            ]
+        ),
+        .tests(
+            domain: .VersionCheckDomain,
+            dependencies: [
+                .domain(target: .VersionCheckDomain, type: .implementation),
+                .domain(target: .VersionCheckDomain, type: .testing),
+            ]
+        ),
+    ]
+)
