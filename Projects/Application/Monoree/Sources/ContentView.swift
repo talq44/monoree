bdec2c96659f1db630e9ca7271a7b11f383c7095
talq44 <talq44@gmail.com>
2025-08-28 21:36:00 +0900

    [#46] 파일 대체

---
+++ Projects/Application/Monoree/Sources/ContentView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    NavigationStack {
+      Text("Hello, Monoree")
+        .padding()
+        .navigationTitle("Home")
+    }
+  }
+}
