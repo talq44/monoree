e3e66636dec3098de1886e702b5889df87017186
talq44 <talq44@gmail.com>
2025-08-28 21:25:58 +0900

    [#46] 줄맞춤

--- Projects/Feature/HomeFeature/Project.swift
+++ Projects/Feature/HomeFeature/Project.swift
@@ -5,35 +5,35 @@ let project = Project.module(
     name: FeatureModule.HomeFeature.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			feature: .HomeFeature,
-			dependencies: []
-		),
-		.implementation(
-			feature: .HomeFeature,
-			dependencies: [
-				.feature(target: .HomeFeature, type: .interface),
-			]
-		),
-		.testing(
-			feature: .HomeFeature,
-			dependencies: [
-				.feature(target: .HomeFeature, type: .interface),
-			]
-		),
-		.tests(
-			feature: .HomeFeature,
-			dependencies: [
-				.feature(target: .HomeFeature, type: .implementation),
-				.feature(target: .HomeFeature, type: .testing),
-			]
-		),
-		.demo(
-			feature: .HomeFeature,
-			dependencies: [
-				.feature(target: .HomeFeature, type: .implementation),
-				.feature(target: .HomeFeature, type: .testing),
-			]
-		),
-]
+        .interface(
+            feature: .HomeFeature,
+            dependencies: []
+        ),
+        .implementation(
+            feature: .HomeFeature,
+            dependencies: [
+                .feature(target: .HomeFeature, type: .interface),
+            ]
+        ),
+        .testing(
+            feature: .HomeFeature,
+            dependencies: [
+                .feature(target: .HomeFeature, type: .interface),
+            ]
+        ),
+        .tests(
+            feature: .HomeFeature,
+            dependencies: [
+                .feature(target: .HomeFeature, type: .implementation),
+                .feature(target: .HomeFeature, type: .testing),
+            ]
+        ),
+        .demo(
+            feature: .HomeFeature,
+            dependencies: [
+                .feature(target: .HomeFeature, type: .implementation),
+                .feature(target: .HomeFeature, type: .testing),
+            ]
+        ),
+    ]
 )
