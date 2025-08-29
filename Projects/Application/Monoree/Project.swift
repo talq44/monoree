8e0a4b992b438b1e48a5b143ab6d21d37a5f6ccd
talq44 <talq44@gmail.com>
2025-08-29 23:29:39 +0900

    [NO-ISSUE] 의존성 추가

--- Projects/Application/Monoree/Project.swift
+++ Projects/Application/Monoree/Project.swift
@@ -36,6 +36,7 @@ let project = Project(
                 // Core
                 .core(target: .UserAPICore, type: .implementation),
                 // Domain
+                .domain(target: .GameCompletionDomain, type: .implementation),
                 .domain(target: .KoreanChosungDomain, type: .implementation),
                 .domain(target: .VersionCheckDomain, type: .implementation),
                 // Feature
