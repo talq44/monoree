1e2f82c08fa8063dfeeea54405e760a0007d5020
talq44 <talq44@gmail.com>
2025-08-29 21:15:25 +0900

    [#52] 의존성 주입

--- Projects/Domain/GameLimitCheckDomain/Project.swift
+++ Projects/Domain/GameLimitCheckDomain/Project.swift
@@ -5,28 +5,29 @@ let project = Project.module(
     name: DomainModule.GameLimitCheckDomain.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			domain: .GameLimitCheckDomain,
-			dependencies: []
-		),
-		.implementation(
-			domain: .GameLimitCheckDomain,
-			dependencies: [
-				.domain(target: .GameLimitCheckDomain, type: .interface),
-			]
-		),
-		.testing(
-			domain: .GameLimitCheckDomain,
-			dependencies: [
-				.domain(target: .GameLimitCheckDomain, type: .interface),
-			]
-		),
-		.tests(
-			domain: .GameLimitCheckDomain,
-			dependencies: [
-				.domain(target: .GameLimitCheckDomain, type: .implementation),
-				.domain(target: .GameLimitCheckDomain, type: .testing),
-			]
-		),
-]
+        .interface(
+            domain: .GameLimitCheckDomain,
+            dependencies: []
+        ),
+        .implementation(
+            domain: .GameLimitCheckDomain,
+            dependencies: [
+                .core(target: .RemoteConfigCore, type: .interface),
+                .domain(target: .GameLimitCheckDomain, type: .interface),
+            ]
+        ),
+        .testing(
+            domain: .GameLimitCheckDomain,
+            dependencies: [
+                .domain(target: .GameLimitCheckDomain, type: .interface),
+            ]
+        ),
+        .tests(
+            domain: .GameLimitCheckDomain,
+            dependencies: [
+                .domain(target: .GameLimitCheckDomain, type: .implementation),
+                .domain(target: .GameLimitCheckDomain, type: .testing),
+            ]
+        ),
+    ]
 )
