c06cf10a8924b5c09a366972f14d76d9b6d47314
talq44 <talq44@gmail.com>
2025-08-29 21:14:03 +0900

    [#52] 1차 추가

---
+++ Projects/Domain/GameLimitCheckDomain/Project.swift
@@ -0,0 +1,32 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: DomainModule.GameLimitCheckDomain.name,
+    settings: .Module.default,
+    targets: [
+		.interface(
+			domain: .GameLimitCheckDomain,
+			dependencies: []
+		),
+		.implementation(
+			domain: .GameLimitCheckDomain,
+			dependencies: [
+				.domain(target: .GameLimitCheckDomain, type: .interface),
+			]
+		),
+		.testing(
+			domain: .GameLimitCheckDomain,
+			dependencies: [
+				.domain(target: .GameLimitCheckDomain, type: .interface),
+			]
+		),
+		.tests(
+			domain: .GameLimitCheckDomain,
+			dependencies: [
+				.domain(target: .GameLimitCheckDomain, type: .implementation),
+				.domain(target: .GameLimitCheckDomain, type: .testing),
+			]
+		),
+]
+)
--- Tuist/ProjectDescriptionHelpers/Modules/DomainModule.swift
+++ Tuist/ProjectDescriptionHelpers/Modules/DomainModule.swift
@@ -1,6 +1,7 @@
 import ProjectDescription
 
 public enum DomainModule: String, CaseIterable {
+	case GameLimitCheckDomain
     case KoreanChosungDomain
     case VersionCheckDomain
     
