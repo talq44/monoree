ea79acb0e04d114d45809519d8bf29d58a8971ad
talq44 <talq44@gmail.com>
2025-08-31 21:53:34 +0900

    [#58] 모듈 추가

---
+++ Projects/Core/LocalDataCore/Project.swift
@@ -0,0 +1,32 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: CoreModule.LocalDataCore.name,
+    settings: .Module.default,
+    targets: [
+        .interface(
+            core: .LocalDataCore,
+            dependencies: []
+        ),
+        .implementation(
+            core: .LocalDataCore,
+            dependencies: [
+                .core(target: .LocalDataCore, type: .interface),
+            ]
+        ),
+        .testing(
+            core: .LocalDataCore,
+            dependencies: [
+                .core(target: .LocalDataCore, type: .interface),
+            ]
+        ),
+        .tests(
+            core: .LocalDataCore,
+            dependencies: [
+                .core(target: .LocalDataCore, type: .implementation),
+                .core(target: .LocalDataCore, type: .testing),
+            ]
+        ),
+    ]
+)
