6fb9cbc5bcafb3e9b43cda21d853e7dd6888f7c9
talq44 <talq44@gmail.com>
2025-09-01 19:49:45 +0900

    [#57] 그래프 변경

--- Projects/Domain/IDFADomain/Sources/Sources.swift
+++ Projects/Domain/IDFADomain/Sources/Sources.swift
@@ -1,5 +1,7 @@
 import Foundation
 import IDFADomainInterface
+import AnalyticsCoreInterface
+import LocalDataCoreInterface
 
 final actor IDFAUsecaseImpl: IDFAUsecase {
     func idfa() async -> String {
