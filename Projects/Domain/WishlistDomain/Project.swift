2cd9124b5b5e3168aecc303e414f7ec44a16b58a
talq44 <talq44@gmail.com>
2025-09-02 22:40:19 +0900

    [#70] 의존성 추가

--- Projects/Domain/WishlistDomain/Project.swift
+++ Projects/Domain/WishlistDomain/Project.swift
@@ -5,28 +5,30 @@ let project = Project.module(
     name: DomainModule.WishlistDomain.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			domain: .WishlistDomain,
-			dependencies: []
-		),
-		.implementation(
-			domain: .WishlistDomain,
-			dependencies: [
-				.domain(target: .WishlistDomain, type: .interface),
-			]
-		),
-		.testing(
-			domain: .WishlistDomain,
-			dependencies: [
-				.domain(target: .WishlistDomain, type: .interface),
-			]
-		),
-		.tests(
-			domain: .WishlistDomain,
-			dependencies: [
-				.domain(target: .WishlistDomain, type: .implementation),
-				.domain(target: .WishlistDomain, type: .testing),
-			]
-		),
-	]
+        .interface(
+            domain: .WishlistDomain,
+            dependencies: []
+        ),
+        .implementation(
+            domain: .WishlistDomain,
+            dependencies: [
+                .core(target: .AnalyticsCore, type: .interface),
+                .core(target: .LocalDataCore, type: .interface),
+                .domain(target: .WishlistDomain, type: .interface),
+            ]
+        ),
+        .testing(
+            domain: .WishlistDomain,
+            dependencies: [
+                .domain(target: .WishlistDomain, type: .interface),
+            ]
+        ),
+        .tests(
+            domain: .WishlistDomain,
+            dependencies: [
+                .domain(target: .WishlistDomain, type: .implementation),
+                .domain(target: .WishlistDomain, type: .testing),
+            ]
+        ),
+    ]
 )
