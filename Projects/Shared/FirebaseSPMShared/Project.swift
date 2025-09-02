f66d789c9deeecbab21289e509f59c121708a694
talq44 <talq44@gmail.com>
2025-09-02 21:57:44 +0900

    [#64] 테스트코드 초기값 변경

--- Projects/Shared/FirebaseSPMShared/Project.swift
+++ Projects/Shared/FirebaseSPMShared/Project.swift
@@ -5,14 +5,9 @@ let project = Project.module(
     name: SharedModule.FirebaseSPMShared.name,
     settings: .Module.default,
     targets: [
-        .interface(
-            shared: .FirebaseSPMShared,
-            dependencies: []
-        ),
         .implementation(
             shared: .FirebaseSPMShared,
             dependencies: [
-                .shared(target: .FirebaseSPMShared, type: .interface),
                 .SPM.firebaseCore,
                 .SPM.firebaseAnalytics,
                 .SPM.firebaseCrashlytics,
