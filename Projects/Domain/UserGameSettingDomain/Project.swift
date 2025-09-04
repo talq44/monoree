66230919dba12843afcbdd846d191369cc552243
talq44 <talq44@gmail.com>
2025-09-04 22:20:53 +0900

    [#77] 설정 추가

---
+++ Projects/Domain/UserGameSettingDomain/Project.swift
@@ -0,0 +1,32 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: DomainModule.UserGameSettingDomain.name,
+    settings: .Module.default,
+    targets: [
+		.interface(
+			domain: .UserGameSettingDomain,
+			dependencies: []
+		),
+		.implementation(
+			domain: .UserGameSettingDomain,
+			dependencies: [
+				.domain(target: .UserGameSettingDomain, type: .interface),
+			]
+		),
+		.testing(
+			domain: .UserGameSettingDomain,
+			dependencies: [
+				.domain(target: .UserGameSettingDomain, type: .interface),
+			]
+		),
+		.tests(
+			domain: .UserGameSettingDomain,
+			dependencies: [
+				.domain(target: .UserGameSettingDomain, type: .implementation),
+				.domain(target: .UserGameSettingDomain, type: .testing),
+			]
+		),
+	]
+)
