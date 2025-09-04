99950c97659e444b3861f369addf70d97c8e1c71
talq44 <talq44@gmail.com>
2025-09-04 23:05:54 +0900

    [#77] mock 일차 구현

---
+++ Projects/Domain/UserGameSettingDomain/Testing/GameConfigManagerImpl.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UserGameSettingDomainInterface
+import RemoteConfigCoreInterface
+
+final class GameConfigManagerImpl: GameConfigManager {
+    func fetchGame() throws -> any RemoteConfigCoreInterface.GameConfigDTO {
+        return GameConfigDTOStub(
+            gamePlaysPerAd: 5,
+            questionCount: 10,
+            timePerQuestion: 10,
+            teamCount: 10
+        )
+    }
+}
+
+struct GameConfigDTOStub: GameConfigDTO {
+    var gamePlaysPerAd: Int
+    var questionCount: Int
+    var timePerQuestion: Int
+    var teamCount: Int
+}
---
+++ Projects/Domain/UserGameSettingDomain/Testing/UserLocalDataManagerImpl.swift
@@ -0,0 +1,22 @@
+import UserGameSettingDomainInterface
+import LocalDataCoreInterface
+
+final class UserLocalDataManagerImpl: UserLocalDataManager {
+    func addWishList(id: String) async throws { }
+    
+    func deleteWishList(id: String) async throws { }
+    
+    func getWishLists() async throws -> [String] { [] }
+    
+    func questionCount() async -> Int? { 3 }
+    
+    func setQuestionCount(_ value: Int) async { }
+    
+    func timePerQuestion() async -> Int? { 3 }
+    
+    func setTimePerQuestion(_ value: Int) async { }
+    
+    func teamCount() async -> Int? { 3 }
+    
+    func setTeamCount(_ value: Int) async { }
+}
