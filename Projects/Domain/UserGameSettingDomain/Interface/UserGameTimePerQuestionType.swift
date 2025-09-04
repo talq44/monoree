2d18e8880eda6e1715ac99863d019f19756363e3
talq44 <talq44@gmail.com>
2025-09-04 22:59:44 +0900

    [#77] 설정값 선언

--- Projects/Domain/UserGameSettingDomain/Interface/UserGameTimePerQuestionType.swift
+++ Projects/Domain/UserGameSettingDomain/Interface/UserGameTimePerQuestionType.swift
@@ -4,6 +4,12 @@ public enum UserGameTimePerQuestionType: Equatable {
     case second(Int)
     case unlimited
     
+    public init?(second: Int?) {
+        guard let second else { return nil }
+        
+        self = .second(second)
+    }
+    
     public init(second: Int) {
         self = .second(second)
     }
--- Projects/Domain/UserGameSettingDomain/Sources/UserGameSettingUsecaseImpl.swift
+++ Projects/Domain/UserGameSettingDomain/Sources/UserGameSettingUsecaseImpl.swift
@@ -4,6 +4,12 @@ import LocalDataCoreInterface
 import RemoteConfigCoreInterface
 
 final actor UserGameSettingUsecaseImpl: UserGameSettingUsecase {
+    private enum Constants {
+        static let questionCount: Int = 10
+        static let timePerQuestion: Int = 5
+        static let teamCount: Int = 1
+    }
+    
     private let remoteConfig: GameConfigManager
     private let localData: UserLocalDataManager
     
@@ -15,19 +21,66 @@ final actor UserGameSettingUsecaseImpl: UserGameSettingUsecase {
         self.localData = localData
     }
     
-    func getConfig() async -> UserGameSettingDomainInterface.UserGameSettingItem {
-        <#code#>
+    func getConfig() async -> UserGameSettingItem {
+        let defaultConfig = getDefaultConfig()
+        
+        let questionCount = await localData.questionCount()
+        let timePerQuestion = await localData.timePerQuestion()
+        let teamCount = await localData.teamCount()
+        
+        return UserGameSettingItem(
+            questionCount: questionCount ?? defaultConfig.questionCount,
+            timePerQuestion: UserGameTimePerQuestionType(
+                second: timePerQuestion
+            ) ?? defaultConfig.timePerQuestion,
+            teamCount: teamCount ?? defaultConfig.teamCount
+        )
+    }
+    
+    private func getDefaultConfig() -> UserGameSettingItem {
+        do {
+            let config = try remoteConfig.fetchGame()
+            
+            return UserGameSettingItem(
+                questionCount: config.questionCount,
+                timePerQuestion: UserGameTimePerQuestionType(
+                    second: config.timePerQuestion
+                ),
+                teamCount: config.teamCount
+            )
+        } catch {
+            return UserGameSettingItem(
+                questionCount: Constants.questionCount,
+                timePerQuestion: UserGameTimePerQuestionType(
+                    second: Constants.timePerQuestion
+                ),
+                teamCount: Constants.teamCount
+            )
+        }
     }
     
     func setQuestionCount(_ count: Int) async {
-        <#code#>
+        await localData.setQuestionCount(count)
     }
     
-    func setTimePerQuestion(_ time: UserGameSettingDomainInterface.UserGameTimePerQuestionType) async {
-        <#code#>
+    func setTimePerQuestion(_ time: UserGameTimePerQuestionType) async {
+        guard let value = time.value else { return }
+        
+        await localData.setTimePerQuestion(value)
     }
     
     func setTeamCount(_ count: Int) async {
-        <#code#>
+        await localData.setTeamCount(count)
+    }
+}
+
+extension UserGameTimePerQuestionType {
+    var value: Int? {
+        switch self {
+        case .second(let int):
+            return int
+        case .unlimited:
+            return nil
+        }
     }
 }
