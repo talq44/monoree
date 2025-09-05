91d4b9b2f2575cdb28781d1cb995a33dcb76f288
talq44 <talq44@gmail.com>
2025-09-05 23:55:29 +0900

    [#79] 의존성 단순화

--- Projects/Domain/GameCompletionDomain/Interface/GameCompletionUseCase.swift
+++ Projects/Domain/GameCompletionDomain/Interface/GameCompletionUseCase.swift
@@ -1,15 +1,6 @@
 import Foundation
-import RemoteConfigCoreInterface
-import AnalyticsCoreInterface
-import LocalDataCoreInterface
 
 public protocol GameCompletionUseCase {
-    init(
-        remoteConfig: GameConfigManager,
-        localData: GameLocalDataManager,
-        analytics: AnalyticsManager
-    )
-    
     func completeGame(
         _ input: any GameCompletionInput
     ) async -> GameCompletionResultType
