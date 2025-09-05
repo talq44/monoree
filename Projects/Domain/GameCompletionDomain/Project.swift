18a686302b43c692ee6e16172bfdffa876676520
talq44 <talq44@gmail.com>
2025-09-05 23:54:33 +0900

    [#79] 의존성 순서 변경

--- Projects/Domain/GameCompletionDomain/Project.swift
+++ Projects/Domain/GameCompletionDomain/Project.swift
@@ -9,19 +9,22 @@ let project = Project.module(
             domain: .GameCompletionDomain,
             dependencies: [
                 .core(target: .RemoteConfigCore, type: .interface),
-                .core(target: .LocalDataCore, type: .interface),
-                .core(target: .AnalyticsCore, type: .interface),
             ]
         ),
         .implementation(
             domain: .GameCompletionDomain,
             dependencies: [
+                .core(target: .LocalDataCore, type: .interface),
+                .core(target: .AnalyticsCore, type: .interface),
                 .domain(target: .GameCompletionDomain, type: .interface),
             ]
         ),
         .testing(
             domain: .GameCompletionDomain,
             dependencies: [
+                .shared(target: .ExtensionsShared),
+                .core(target: .LocalDataCore, type: .interface),
+                .core(target: .AnalyticsCore, type: .interface),
                 .domain(target: .GameCompletionDomain, type: .interface),
             ]
         ),
