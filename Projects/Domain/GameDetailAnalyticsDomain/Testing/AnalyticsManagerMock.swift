575de2b3d125fa12739c1bacd3de012bafe985de
talq44 <talq44@gmail.com>
2025-09-05 22:51:13 +0900

    [#79] 모킹 기본 구조 선언

---
+++ Projects/Domain/GameDetailAnalyticsDomain/Testing/AnalyticsManagerMock.swift
@@ -0,0 +1,13 @@
+import Foundation
+import GameDetailAnalyticsDomainInterface
+import AnalyticsCoreInterface
+
+class AnalyticsManagerMock: AnalyticsManager {
+    func setUserId(id: String?) {
+        
+    }
+    
+    func sendEvent(_ event: AnalyticsCoreInterface.AnalyticsEvent) {
+        
+    }
+}
