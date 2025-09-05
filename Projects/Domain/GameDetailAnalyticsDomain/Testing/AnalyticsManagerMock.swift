8ce34fb26ece490b5dbbd696255d0ce643650ff4
talq44 <talq44@gmail.com>
2025-09-05 22:59:03 +0900

    [#79] 테스트코드 추가

--- Projects/Domain/GameDetailAnalyticsDomain/Testing/AnalyticsManagerMock.swift
+++ Projects/Domain/GameDetailAnalyticsDomain/Testing/AnalyticsManagerMock.swift
@@ -1,13 +1,19 @@
-import Foundation
-import GameDetailAnalyticsDomainInterface
 import AnalyticsCoreInterface
+import Foundation
 
 class AnalyticsManagerMock: AnalyticsManager {
+    private(set) var setUserIdCalledWith: String? = nil
+    private(set) var sendEventCalledWith: AnalyticsEvent? = nil
+    private(set) var sendEventCallCount: Int = 0
+    private(set) var setUserIdCallCount: Int = 0
+    
     func setUserId(id: String?) {
-        
+        setUserIdCalledWith = id
+        setUserIdCallCount += 1
     }
     
     func sendEvent(_ event: AnalyticsCoreInterface.AnalyticsEvent) {
-        
+        sendEventCalledWith = event
+        sendEventCallCount += 1
     }
 }
