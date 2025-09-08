6ea0fbc54e7a6ad6a7ffb2aac167fe32d31064e3
talq44 <talq44@gmail.com>
2025-09-08 23:06:19 +0900

    [#86] 의존성 추가

--- Projects/Feature/IntroFeature/Project.swift
+++ Projects/Feature/IntroFeature/Project.swift
@@ -5,35 +5,37 @@ let project = Project.module(
     name: FeatureModule.IntroFeature.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			feature: .IntroFeature,
-			dependencies: []
-		),
-		.implementation(
-			feature: .IntroFeature,
-			dependencies: [
-				.feature(target: .IntroFeature, type: .interface),
-			]
-		),
-		.testing(
-			feature: .IntroFeature,
-			dependencies: [
-				.feature(target: .IntroFeature, type: .interface),
-			]
-		),
-		.tests(
-			feature: .IntroFeature,
-			dependencies: [
-				.feature(target: .IntroFeature, type: .implementation),
-				.feature(target: .IntroFeature, type: .testing),
-			]
-		),
-		.demo(
-			feature: .IntroFeature,
-			dependencies: [
-				.feature(target: .IntroFeature, type: .implementation),
-				.feature(target: .IntroFeature, type: .testing),
-			]
-		),
-	]
+        .interface(
+            feature: .IntroFeature,
+            dependencies: []
+        ),
+        .implementation(
+            feature: .IntroFeature,
+            dependencies: [
+                .domain(target: .VersionCheckDomain, type: .interface),
+                .feature(target: .IntroFeature, type: .interface),
+            ]
+        ),
+        .testing(
+            feature: .IntroFeature,
+            dependencies: [
+                .domain(target: .VersionCheckDomain, type: .interface),
+                .feature(target: .IntroFeature, type: .interface),
+            ]
+        ),
+        .tests(
+            feature: .IntroFeature,
+            dependencies: [
+                .feature(target: .IntroFeature, type: .implementation),
+                .feature(target: .IntroFeature, type: .testing),
+            ]
+        ),
+        .demo(
+            feature: .IntroFeature,
+            dependencies: [
+                .feature(target: .IntroFeature, type: .implementation),
+                .feature(target: .IntroFeature, type: .testing),
+            ]
+        ),
+    ]
 )
