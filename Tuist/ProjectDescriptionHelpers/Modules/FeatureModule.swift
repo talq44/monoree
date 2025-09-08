7828ac7f376d5d40643acbb0c4b3e3eec556581b
talq44 <talq44@gmail.com>
2025-09-08 23:05:08 +0900

    [#86] sort

--- Tuist/ProjectDescriptionHelpers/Modules/FeatureModule.swift
+++ Tuist/ProjectDescriptionHelpers/Modules/FeatureModule.swift
@@ -1,9 +1,9 @@
 import ProjectDescription
 
 public enum FeatureModule: String, CaseIterable {
-	case IntroFeature
-	case HomeFeature
     case GamePlayFeature
+    case HomeFeature
+	case IntroFeature
     case TimerFeature
     
     public var name: String {
