dedec50363faa45100601b8973eca9edc94853ec
talq44 <talq44@gmail.com>
2025-09-09 22:34:20 +0900

    [#87] 리소스 추가

--- Projects/Feature/IntroFeature/Project.swift
+++ Projects/Feature/IntroFeature/Project.swift
@@ -11,6 +11,7 @@ let project = Project.module(
         ),
         .implementation(
             feature: .IntroFeature,
+            hasResources: true,
             dependencies: [
                 // SPM
                 .SPM.ComposableArchitecture,
