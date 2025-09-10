747787661dbc25e1c65281f2bc1a62aaf98388f4
talq44 <talq44@gmail.com>
2025-09-10 23:39:58 +0900

    [#89] 의존성 선언

--- Projects/Core/MonoreeDesignSystemCore/Project.swift
+++ Projects/Core/MonoreeDesignSystemCore/Project.swift
@@ -5,28 +5,29 @@ let project = Project.module(
     name: CoreModule.MonoreeDesignSystemCore.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			core: .MonoreeDesignSystemCore,
-			dependencies: []
-		),
-		.implementation(
-			core: .MonoreeDesignSystemCore,
-			dependencies: [
-				.core(target: .MonoreeDesignSystemCore, type: .interface),
-			]
-		),
-		.testing(
-			core: .MonoreeDesignSystemCore,
-			dependencies: [
-				.core(target: .MonoreeDesignSystemCore, type: .interface),
-			]
-		),
-		.tests(
-			core: .MonoreeDesignSystemCore,
-			dependencies: [
-				.core(target: .MonoreeDesignSystemCore, type: .implementation),
-				.core(target: .MonoreeDesignSystemCore, type: .testing),
-			]
-		),
-	]
+        .interface(
+            core: .MonoreeDesignSystemCore,
+            dependencies: []
+        ),
+        .implementation(
+            core: .MonoreeDesignSystemCore,
+            dependencies: [
+                .shared(target: .UIThirdKit),
+                .core(target: .MonoreeDesignSystemCore, type: .interface),
+            ]
+        ),
+        .testing(
+            core: .MonoreeDesignSystemCore,
+            dependencies: [
+                .core(target: .MonoreeDesignSystemCore, type: .interface),
+            ]
+        ),
+        .tests(
+            core: .MonoreeDesignSystemCore,
+            dependencies: [
+                .core(target: .MonoreeDesignSystemCore, type: .implementation),
+                .core(target: .MonoreeDesignSystemCore, type: .testing),
+            ]
+        ),
+    ]
 )
