6790e2b06689a5a23dc35658d0305d1c231a7fda
talq44 <talq44@gmail.com>
2025-09-10 23:38:48 +0900

    [#89] 디자인 시스템 추가

---
+++ Projects/Core/MonoreeDesignSystemCore/Project.swift
@@ -0,0 +1,32 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: CoreModule.MonoreeDesignSystemCore.name,
+    settings: .Module.default,
+    targets: [
+		.interface(
+			core: .MonoreeDesignSystemCore,
+			dependencies: []
+		),
+		.implementation(
+			core: .MonoreeDesignSystemCore,
+			dependencies: [
+				.core(target: .MonoreeDesignSystemCore, type: .interface),
+			]
+		),
+		.testing(
+			core: .MonoreeDesignSystemCore,
+			dependencies: [
+				.core(target: .MonoreeDesignSystemCore, type: .interface),
+			]
+		),
+		.tests(
+			core: .MonoreeDesignSystemCore,
+			dependencies: [
+				.core(target: .MonoreeDesignSystemCore, type: .implementation),
+				.core(target: .MonoreeDesignSystemCore, type: .testing),
+			]
+		),
+	]
+)
