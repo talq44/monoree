44c0ecbc85df71b94671fa53e46c7a9bdef65718
talq44 <talq44@gmail.com>
2025-09-21 12:24:44 +0900

    [#51] 형태 선언

--- Projects/Core/AdMobCore/Project.swift
+++ Projects/Core/AdMobCore/Project.swift
@@ -5,28 +5,21 @@ let project = Project.module(
     name: CoreModule.AdMobCore.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			core: .AdMobCore,
-			dependencies: []
-		),
-		.implementation(
-			core: .AdMobCore,
-			dependencies: [
-				.core(target: .AdMobCore, type: .interface),
-			]
-		),
-		.testing(
-			core: .AdMobCore,
-			dependencies: [
-				.core(target: .AdMobCore, type: .interface),
-			]
-		),
-		.tests(
-			core: .AdMobCore,
-			dependencies: [
-				.core(target: .AdMobCore, type: .implementation),
-				.core(target: .AdMobCore, type: .testing),
-			]
-		),
-	]
+        .interface(
+            core: .AdMobCore,
+            dependencies: []
+        ),
+        .implementation(
+            core: .AdMobCore,
+            dependencies: [
+                .core(target: .AdMobCore, type: .interface),
+            ]
+        ),
+        .demo(
+            core: .AdMobCore,
+            dependencies: [
+                .core(target: .AdMobCore, type: .implementation)
+            ]
+        )
+    ]
 )
