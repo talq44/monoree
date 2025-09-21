6a446a88a98247471a55272c9952d67cf56f5212
talq44 <talq44@gmail.com>
2025-09-21 17:53:34 +0900

    [#51] 초기화 위치 수정

--- Projects/Core/AdMobCore/Sources/BannerContentView.swift
+++ Projects/Core/AdMobCore/Sources/BannerContentView.swift
@@ -1,38 +1,17 @@
 import SwiftUI
 import GoogleMobileAds
 
-struct BannerContentView: View {
-    let navigationTitle: String
-    
-    // [START add_banner_to_view]
-    var body: some View {
-        Spacer()
-        // Request an anchored adaptive banner with a width of 375.
-        let adSize = currentOrientationAnchoredAdaptiveBanner(width: 375)
-        BannerViewContainer(adSize)
-            .frame(width: adSize.size.width, height: adSize.size.height)
-        // [START_EXCLUDE silent]
-            .navigationTitle(navigationTitle)
-        // [END_EXCLUDE]
-    }
-    // [END add_banner_to_view]
-}
-
-#Preview {
-    BannerContentView(navigationTitle: "Banner")
-}
-
 // [START create_banner_view]
-private struct BannerViewContainer: UIViewRepresentable {
+struct BannerViewContainer: UIViewRepresentable {
     typealias UIViewType = BannerView
     let adSize: AdSize
     let adUnitID: String
     
     init(
-        _ adSize: AdSize,
-        adUnitID: String = "ca-app-pub-3940256099942544/2435281174"
+        _ width: CGFloat,
+        adUnitID: String
     ) {
-        self.adSize = adSize
+        self.adSize = currentOrientationAnchoredAdaptiveBanner(width: width)
         self.adUnitID = adUnitID
     }
     
@@ -73,4 +52,8 @@ private struct BannerViewContainer: UIViewRepresentable {
             print("FAILED TO RECEIVE AD: \(error.localizedDescription)")
         }
     }
+}
+
+#Preview {
+    BannerViewContainer(375, adUnitID: "ca-app-pub-3940256099942544/2435281174")
 }
