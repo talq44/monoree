5ca2d56b37da8fe04edc9141953933f54a79041d
talq44 <talq44@gmail.com>
2025-10-13 20:46:13 +0900

    [#109] stackView들 추가

--- Projects/Shared/UIKitExtensionShared/Sources/VStackView.swift
+++ Projects/Shared/UIKitExtensionShared/Sources/VStackView.swift
@@ -1,20 +1,19 @@
-//
-//  VStackView.swift
-//  UIKitExtensionShared
-//
-//  Created by 박창규 on 10/13/25.
-//
-
 import UIKit
 
-class VStackView: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+public class VStackView: UIStackView {
+    public init(
+        spacing: CGFloat = 0,
+        distribution: UIStackView.Distribution = .fill,
+        alignment: UIStackView.Alignment = .fill,
+        frame: CGRect = .zero
+    ) {
+        super.init(frame: frame)
+        
+        self.axis = .vertical
+        self.spacing = spacing
     }
-    */
-
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
