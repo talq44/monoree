6216bc384c790d1cec40ac3102ba9515603d36bd
talq44 <talq44@gmail.com>
2025-10-13 20:33:49 +0900

    [#109] 최소 연결

---
+++ Projects/Feature/AnimalQuizLabFeature/Demo/Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+import UIKit
+import AnimalQuizLabFeature
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let vc = AQLCoordinator.home()
+        let nc = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nc
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
