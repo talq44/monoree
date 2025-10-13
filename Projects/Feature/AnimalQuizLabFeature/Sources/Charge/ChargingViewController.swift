bdd4bc0824c7a46823a34add1d92201905558226
talq44 <talq44@gmail.com>
2025-10-13 21:28:37 +0900

    [#109] 충전소 화면 연결

--- Projects/Feature/AnimalQuizLabFeature/Sources/Charge/ChargingViewController.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/Charge/ChargingViewController.swift
@@ -1,5 +1,10 @@
 import UIKit
 import UIKitExtensionShared
+import FoundationShared
+import SnapKit
+import RxSwift
+import RxCocoa
+import ReactorKit
 
 final class ChargingViewController: BaseViewController {
     private let stackView = VStackView()
@@ -11,14 +16,23 @@ final class ChargingViewController: BaseViewController {
     private let chargeCoinTitleLabel = BaseLabel("충전 코인",style: .title3)
     private let chargeCoinValueLabel = BaseLabel("0", style: .title3)
     
+    init(reactor: ChargingViewReactor = .init()) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    @MainActor required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         super.loadView()
         
         self.view.addSubview(stackView)
         
         stackView.snp.makeConstraints { make in
+            make.directionalVerticalEdges.equalTo(view.safeAreaLayoutGuide)
             make.directionalHorizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(Spacing.l)
-            make.directionalEdges.equalTo(view.safeAreaLayoutGuide)
         }
         
         stackView.addArrangedSubviews(
@@ -36,5 +50,23 @@ final class ChargingViewController: BaseViewController {
         super.viewDidLoad()
         
         self.title = "충전소"
+    }
+}
+
+extension ChargingViewController: ReactorKit.View {
+    typealias Reactor = ChargingViewReactor
+    
+    func bind(reactor: ChargingViewReactor) {
+        bindState(state: reactor.state.distinctUntilChanged())
+    }
+    
+    func bindState(state: Observable<Reactor.State>) {
+        state.map { $0.freeCoin.decimalString }
+            .bind(to: freeCoinValueLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        state.map { $0.chargeCoin.decimalString }
+            .bind(to: chargeCoinValueLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 }
