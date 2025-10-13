9ddce73d0d821fd9937ebd7c92e784943c23bbb4
talq44 <talq44@gmail.com>
2025-10-13 21:28:45 +0900

    [#109] reactor 추가

--- Projects/Feature/AnimalQuizLabFeature/Sources/Charge/ChargingViewReactor.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/Charge/ChargingViewReactor.swift
@@ -1,8 +1,23 @@
-//
-//  ChargingViewReactor.swift
-//  AnimalQuizLabFeature
-//
-//  Created by 박창규 on 10/13/25.
-//
-
 import Foundation
+import ReactorKit
+import RxSwift
+
+enum ChargingViewAction {
+    
+}
+
+struct ChargingViewState: Equatable {
+    var freeCoin: Int = 3
+    var chargeCoin: Int = 0
+}
+
+final class ChargingViewReactor: Reactor {
+    typealias Action = ChargingViewAction
+    typealias State = ChargingViewState
+    
+    let initialState: ChargingViewState
+    
+    init() {
+        initialState = .init()
+    }
+}
