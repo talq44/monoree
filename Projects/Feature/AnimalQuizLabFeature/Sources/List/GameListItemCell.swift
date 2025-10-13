64b06ac479a07c292c416d14657d305b6fa0f3c5
talq44 <talq44@gmail.com>
2025-10-13 23:31:15 +0900

    [#108] 게임 목록 화면 추가

--- Projects/Feature/AnimalQuizLabFeature/Sources/List/GameListItemCell.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/List/GameListItemCell.swift
@@ -3,31 +3,42 @@ import SnapKit
 import UIKitExtensionShared
 
 final class GameListItemCell: UITableViewCell {
-    private let backgroundImageView = UIImageView()
     private let stackView = HStackView(spacing: Spacing.s, alignment: .center)
     private let leftImageView = UIImageView()
-    private let titleLabel = BaseLabel(style: .title2)
+    private let titleLabel = BaseLabel(style: .title3)
     private let rightImageView = UIImageView(image: UIImage(systemName: "chevron.right"))
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        self.selectionStyle = .none
+        self.backgroundColor = .clear
+        
         contentView.addSubview(stackView)
-        contentView.addSubview(backgroundImageView)
+        
+        stackView.layer.cornerRadius = 8
+        stackView.layer.borderWidth = 1
+        stackView.layer.borderColor = UIColor.separator.cgColor
         
         stackView.addArrangedSubviews(
+            SpacerView(width: 0),
             leftImageView,
             titleLabel,
             SpacerView(),
-            rightImageView
+            rightImageView,
+            SpacerView(width: 0)
         )
+        
+        stackView.snp.makeConstraints { make in
+            make.directionalEdges.equalToSuperview().inset(Spacing.xs)
+        }
         
         leftImageView.snp.makeConstraints { make in
             make.size.equalTo(48)
         }
         
         rightImageView.snp.makeConstraints { make in
-            make.size.equalTo(24)
+            make.size.equalTo(CGSize(width: 12, height: 20))
         }
     }
     
@@ -40,3 +51,4 @@ final class GameListItemCell: UITableViewCell {
         leftImageView.image = state.leftImage
     }
 }
+
