86b1594540fe9bebad2cc10c91bc794c75bca283
talq44 <talq44@gmail.com>
2025-10-13 20:36:38 +0900

    [#109] 사용하지 않는 파일 삭제

--- Projects/Shared/UIKitExtensionShared/Project.swift
+++ Projects/Shared/UIKitExtensionShared/Project.swift
@@ -5,28 +5,11 @@ let project = Project.module(
     name: SharedModule.UIKitExtensionShared.name,
     settings: .Module.default,
     targets: [
-		.interface(
-			shared: .UIKitExtensionShared,
-			dependencies: []
-		),
 		.implementation(
 			shared: .UIKitExtensionShared,
 			dependencies: [
 				.shared(target: .UIKitExtensionShared, type: .interface),
 			]
-		),
-		.testing(
-			shared: .UIKitExtensionShared,
-			dependencies: [
-				.shared(target: .UIKitExtensionShared, type: .interface),
-			]
-		),
-		.tests(
-			shared: .UIKitExtensionShared,
-			dependencies: [
-				.shared(target: .UIKitExtensionShared, type: .implementation),
-				.shared(target: .UIKitExtensionShared, type: .testing),
-			]
-		),
+		)
 	]
 )
