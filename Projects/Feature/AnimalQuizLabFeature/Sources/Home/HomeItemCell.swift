9f91040119c7bef65a96e1ae0e8cd53d35900e6c
talq44 <talq44@gmail.com>
2025-10-14 21:31:09 +0900

    [#118] cell 추가

--- Projects/Feature/AnimalQuizLabFeature/Sources/Home/HomeItemCell.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/Home/HomeItemCell.swift
@@ -1,12 +1,42 @@
-//
-//  HomeItemCell.swift
-//  AnimalQuizLabFeature
-//
-//  Created by 박창규 on 10/14/25.
-//
-
 import UIKit
+import SnapKit
+import UIKitExtensionShared
 
-class HomeItemCell: UICollectionViewCell {
+final class HomeItemCell: UICollectionViewCell {
+    private let stackView = HStackView(spacing: Spacing.s, alignment: .center)
+    private let imageView = UIImageView()
+    private let titlesStackView = VStackView(spacing: Spacing.xs)
+    private let titleLabel = BaseLabel(style: .title3)
+    private let subTitleLabel = BaseLabel(style: .body)
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(stackView)
+        
+        stackView.addArrangedSubviews(
+            SpacerView(width: Spacing.l),
+            titlesStackView.addArrangedSubviews(
+                titleLabel,
+                subTitleLabel
+            ),
+            SpacerView(),
+            UIImageView(image: UIImage(systemName: "chevron.right")),
+            SpacerView(width: Spacing.l)
+        )
+        
+        stackView.snp.makeConstraints { make in
+            make.directionalEdges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func bind(state: HomeViewState.Item) {
+        titleLabel.text = state.title
+        subTitleLabel.text = state.subTitle
+//        contentView.backgroundColor = state.backgroundColor
+    }
 }
