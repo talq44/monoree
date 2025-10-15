0e40c58214ebd1495babec2184a8b3a5a3295bb6
talq44 <talq44@gmail.com>
2025-10-15 22:33:02 +0900

    [#120] background 통일

--- Projects/Feature/AnimalQuizLabFeature/Sources/Base/BaseViewController.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/Base/BaseViewController.swift
@@ -7,7 +7,7 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemGroupedBackground
     }
     
     internal func setupNavigationBarCoin() {
