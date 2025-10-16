03d2aa13f214bab918ff93a72da0f9be6d424ed6
talq44 <talq44@gmail.com>
2025-10-16 21:16:43 +0900

    [#105] 게임 콘텐트 선언

--- Projects/Feature/AnimalQuizLabFeature/Sources/Game/GameContentView.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/Game/GameContentView.swift
@@ -1,20 +1,169 @@
-//
-//  GameContentView.swift
-//  AnimalQuizLabFeature
-//
-//  Created by 박창규 on 10/16/25.
-//
-
 import UIKit
+import SnapKit
+import UIKitExtensionShared
+import Kingfisher
 
-class GameContentView: UIView {
+enum GameContentStyle {
+    case image
+    case text
+    case autoScroll
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+enum GameQuestion {
+    case image(url: String)
+    case text(String)
+}
+
+enum GameAnserType: Int {
+    case answer2 = 2
+    case answer3 = 3
+    case answer4 = 4
+    case noAnswer = 0
+}
+
+final class GameContentView: UIView {
+    struct State {
+        let gameQuestion: GameQuestion
+        let answers: [String]
+        let didSelectAnswer: ((Int) -> Void)
     }
-    */
-
+    
+    private let stackView = VStackView(spacing: Spacing.m, distribution: .fillEqually)
+    private let topView = UIView()
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private let questionLabel = BaseLabel(style: .extraLargeTitle, isMultipleLines: true)
+    private let bottomStackView = VStackView(spacing: Spacing.m, distribution: .fillEqually)
+    private var answerButtons: [UIButton] = []
+    
+    private var didSelectAnswer: ((Int) -> Void)?
+    
+    init(style: GameContentStyle, answer: GameAnserType) {
+        super.init(frame: .zero)
+        
+        addSubview(stackView)
+        
+        stackView.addArrangedSubviews(
+            topView.addSubviews(
+                imageView,
+                questionLabel
+            ),
+            bottomStackView
+        )
+        
+        imageView.contentStyle()
+        
+        stackView.snp.makeConstraints { make in
+            make.directionalEdges.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.directionalEdges.equalToSuperview().inset(Spacing.s)
+        }
+        
+        questionLabel.snp.makeConstraints { make in
+            make.directionalEdges.equalToSuperview().inset(Spacing.s)
+        }
+        
+        switch answer {
+        case .answer2, .answer3:
+            let answerStackView = HStackView(spacing: Spacing.m, distribution: .fillEqually)
+            bottomStackView.addArrangedSubviews(answerStackView)
+            
+            Array(0..<answer.rawValue).forEach { row in
+                let configuration: UIButton.Configuration
+                if #available(iOS 26.0, *) {
+                    configuration = UIButton.Configuration.glass()
+                } else {
+                    configuration = UIButton.Configuration.borderedProminent()
+                }
+                
+                let button = UIButton(configuration: configuration)
+                button.addAction(
+                    UIAction(handler: { [weak self] _ in
+                        self?.didSelectAnswer?(row)
+                    }),
+                    for: .touchUpInside
+                )
+                answerButtons.append(button)
+                answerStackView.addArrangedSubview(button)
+            }
+        case .answer4:
+            let answerStackView = HStackView(spacing: Spacing.m, distribution: .fillEqually)
+            let answerStackView2 = HStackView(spacing: Spacing.m, distribution: .fillEqually)
+            bottomStackView.addArrangedSubviews(
+                answerStackView,
+                answerStackView2
+            )
+            
+            Array(0..<(answer.rawValue / 2)).forEach { row in
+                let configuration: UIButton.Configuration
+                if #available(iOS 26.0, *) {
+                    configuration = UIButton.Configuration.glass()
+                } else {
+                    configuration = UIButton.Configuration.borderedProminent()
+                }
+                
+                let button = UIButton(configuration: configuration)
+                button.addAction(
+                    UIAction(handler: { [weak self] _ in
+                        self?.didSelectAnswer?(row)
+                    }),
+                    for: .touchUpInside
+                )
+                answerButtons.append(button)
+                answerStackView.addArrangedSubview(button)
+            }
+            
+            Array((answer.rawValue / 2)..<answer.rawValue).forEach { row in
+                let configuration: UIButton.Configuration
+                if #available(iOS 26.0, *) {
+                    configuration = UIButton.Configuration.glass()
+                } else {
+                    configuration = UIButton.Configuration.borderedProminent()
+                }
+                
+                let button = UIButton(configuration: configuration)
+                button.addAction(
+                    UIAction(handler: { [weak self] _ in
+                        self?.didSelectAnswer?(row)
+                    }),
+                    for: .touchUpInside
+                )
+                answerButtons.append(button)
+                answerStackView2.addArrangedSubview(button)
+            }
+        case .noAnswer:
+            bottomStackView.isHidden = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func bind(state: State) {
+        switch state.gameQuestion {
+        case .image(let url):
+            imageView.kf.setImage(
+                with: URL(string: url),
+                placeholder: UIImage(systemName: "photo"),
+                options: [.transition(.fade(0.2))]
+            )
+            
+        case .text(let text):
+            questionLabel.text = text
+        }
+        
+        state.answers.enumerated().forEach { index, offset in
+            guard index < answerButtons.count else { return }
+            
+            answerButtons[index].configuration?.title = offset
+        }
+        
+        didSelectAnswer = state.didSelectAnswer
+    }
 }
