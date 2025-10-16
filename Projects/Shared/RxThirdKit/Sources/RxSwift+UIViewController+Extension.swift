01775bb442537cf9e1f2865a9fabd1157a152cbe
talq44 <talq44@gmail.com>
2025-10-16 20:27:00 +0900

    [#105] 줄맞춤

--- Projects/Shared/RxThirdKit/Sources/RxSwift+UIViewController+Extension.swift
+++ Projects/Shared/RxThirdKit/Sources/RxSwift+UIViewController+Extension.swift
@@ -8,46 +8,46 @@ public extension Reactive where Base: UIViewController {
             .map { _ in () }
         return ControlEvent(events: source)
     }
-
+    
     var viewWillAppear: ControlEvent<Bool> {
         let source = self.sentMessage(#selector(UIViewController.viewWillAppear(_:)))
             .compactMap { $0.first as? Bool }
         return ControlEvent(events: source)
     }
-
+    
     var viewDidAppear: ControlEvent<Bool> {
         let source = self.sentMessage(#selector(UIViewController.viewDidAppear(_:)))
             .compactMap { $0.first as? Bool }
         return ControlEvent(events: source)
     }
-
+    
     var viewWillDisappear: ControlEvent<Bool> {
         let source = self.sentMessage(#selector(UIViewController.viewWillDisappear(_:)))
             .compactMap { $0.first as? Bool }
         return ControlEvent(events: source)
     }
-
+    
     var viewDidDisappear: ControlEvent<Bool> {
         let source = self.sentMessage(#selector(UIViewController.viewDidDisappear(_:)))
             .compactMap { $0.first as? Bool }
         return ControlEvent(events: source)
     }
-
+    
     var viewWillAppearVoid: ControlEvent<Void> {
         let source = viewWillAppear.map { _ in () }
         return ControlEvent(events: source)
     }
-
+    
     var viewDidAppearVoid: ControlEvent<Void> {
         let source = viewDidAppear.map { _ in () }
         return ControlEvent(events: source)
     }
-
+    
     var viewWillDisappearVoid: ControlEvent<Void> {
         let source = viewWillDisappear.map { _ in () }
         return ControlEvent(events: source)
     }
-
+    
     var viewDidDisappearVoid: ControlEvent<Void> {
         let source = viewDidDisappear.map { _ in () }
         return ControlEvent(events: source)
