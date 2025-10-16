9531d7f4f28506fc1c2c46113883f14437da0e95
talq44 <talq44@gmail.com>
2025-10-16 21:31:43 +0900

    [#105] 라운딩 처리

--- Projects/Feature/AnimalQuizLabFeature/Sources/Extension/UIView+Extension.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/Extension/UIView+Extension.swift
@@ -3,7 +3,7 @@ import UIKitExtensionShared
 
 extension UIView {
     func contentStyle() {
-        layer.cornerRadius = Spacing.m
+        layer.cornerRadius = Spacing.l
         layer.masksToBounds = true
         backgroundColor = .systemBackground
     }
