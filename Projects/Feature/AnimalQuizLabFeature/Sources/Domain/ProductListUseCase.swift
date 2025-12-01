17561b2a88d3654baed70a82038d74427a62bddc
talq44 <talq44@gmail.com>
2025-12-01 20:07:33 +0900

    [#144] mock 데이터 별도 분리

--- Projects/Feature/AnimalQuizLabFeature/Sources/Domain/ProductListUseCase.swift
+++ Projects/Feature/AnimalQuizLabFeature/Sources/Domain/ProductListUseCase.swift
@@ -13,6 +13,125 @@ final class ProductListUseCase {
             }
         }
         
+        let mock = mockData()
+        
+        // Cache and return filtered
+        cache = mock
+        return cache.filter { item in
+            let matchCategory = category.map { $0 == item.category } ?? true
+            let matchSub = itemCategory2.map { $0 == item.itemCategory2 } ?? true
+            return matchCategory && matchSub
+        }
+    }
+    
+    func executePlay(
+        withType: GameType,
+        questionCount: Int,
+        choicesCount: Int
+    ) async throws -> [GameItem] {
+        guard cache.count > 0 else {
+            _ = try await fetch(category: nil, itemCategory2: nil)
+            return try await executePlay(
+                withType: withType,
+                questionCount: questionCount,
+                choicesCount: choicesCount
+            )
+        }
+        
+        switch withType {
+        case .image, .text, .autoScroll:
+            return executeCountStyle(
+                withType: withType,
+                items: cache,
+                questionCount: questionCount,
+                choicesCount: choicesCount
+            )
+        case .categoryDifferent:
+            return executeCategory2Style(
+                withType: withType,
+                items: cache,
+                questionCount: questionCount,
+                choicesCount: choicesCount
+            )
+        }
+    }
+    
+    private func executeCountStyle(
+        withType: GameType,
+        items: [ProductItem],
+        questionCount: Int,
+        choicesCount: Int
+    ) -> [GameItem] {
+        let selected: [ProductItem] = items.shuffled(count: questionCount)
+        let choiceLimitedCount = max(0, min(choicesCount - 1, items.count - 1))
+        var choices: [[ProductItem]] = []
+        
+        for index in 0..<questionCount {
+            let answer = selected[index]
+            let choicesFilter = items.filter { $0.id != answer.id }
+            
+            if choiceLimitedCount == choicesFilter.count {
+                let shuffled = Array(choicesFilter.shuffled()) + [answer]
+                choices.append(shuffled.shuffled())
+            } else {
+                let shuffled = Array(choicesFilter.shuffled().prefix(choiceLimitedCount)) + [answer]
+                choices.append(shuffled.shuffled())
+            }
+        }
+        
+        return selected.enumerated().map {
+            return GameItem(
+                type: withType,
+                question: $0.element,
+                answer: $0.element,
+                choices: choices[$0.offset]
+            )
+        }
+    }
+    
+    private func executeCategory2Style(
+        withType: GameType,
+        items: [ProductItem],
+        questionCount: Int,
+        choicesCount: Int
+    ) -> [GameItem] {
+        let selected: [ProductItem] = items.shuffled(count: questionCount)
+        var answers: [ProductItem] = []
+        var choices: [[ProductItem]] = []
+        
+        for index in 0..<questionCount {
+            let origin = selected[index]
+            let originFilter = items.filter { $0.id != origin.id }
+            
+            let equals = originFilter.filter { $0.itemCategory2 == origin.itemCategory2 }
+                .shuffled(count: choicesCount - 1)
+            let notEqual = originFilter.filter { $0.itemCategory2 != origin.itemCategory2 }
+                .shuffled()
+                .first
+            
+            guard let notEqual else {
+                answers.append(ProductItem(id: "", names: [], category: "", itemCategory2: ""))
+                choices.append(equals)
+                continue
+            }
+            
+            answers.append(notEqual)
+            choices.append((equals + [notEqual]).shuffled())
+        }
+        
+        return selected.enumerated().map {
+            return GameItem(
+                type: withType,
+                question: $0.element,
+                answer: answers[$0.offset],
+                choices: choices[$0.offset]
+            )
+        }
+    }
+}
+
+extension ProductListUseCase {
+    private func mockData() -> [ProductItem] {
         let baseCategory = "동물"
         let mock: [ProductItem] = [
             ProductItem(
@@ -294,119 +413,9 @@ final class ProductListUseCase {
             ),
         ]
         
-        // Cache and return filtered
-        cache = mock
-        return cache.filter { item in
-            let matchCategory = category.map { $0 == item.category } ?? true
-            let matchSub = itemCategory2.map { $0 == item.itemCategory2 } ?? true
-            return matchCategory && matchSub
-        }
+        return mock
     }
-    
-    func executePlay(
-        withType: GameType,
-        questionCount: Int,
-        choicesCount: Int
-    ) async throws -> [GameItem] {
-        guard cache.count > 0 else {
-            _ = try await fetch(category: nil, itemCategory2: nil)
-            return try await executePlay(
-                withType: withType,
-                questionCount: questionCount,
-                choicesCount: choicesCount
-            )
-        }
-        
-        switch withType {
-        case .image, .text, .autoScroll:
-            return executeCountStyle(
-                withType: withType,
-                items: cache,
-                questionCount: questionCount,
-                choicesCount: choicesCount
-            )
-        case .categoryDifferent:
-            return executeCategory2Style(
-                withType: withType,
-                items: cache,
-                questionCount: questionCount,
-                choicesCount: choicesCount
-            )
-        }
-    }
-    
-    private func executeCountStyle(
-        withType: GameType,
-        items: [ProductItem],
-        questionCount: Int,
-        choicesCount: Int
-    ) -> [GameItem] {
-        let selected: [ProductItem] = items.shuffled(count: questionCount)
-        let choiceLimitedCount = max(0, min(choicesCount - 1, items.count - 1))
-        var choices: [[ProductItem]] = []
-        
-        for index in 0..<questionCount {
-            let answer = selected[index]
-            let choicesFilter = items.filter { $0.id != answer.id }
-            
-            if choiceLimitedCount == choicesFilter.count {
-                let shuffled = Array(choicesFilter.shuffled()) + [answer]
-                choices.append(shuffled.shuffled())
-            } else {
-                let shuffled = Array(choicesFilter.shuffled().prefix(choiceLimitedCount)) + [answer]
-                choices.append(shuffled.shuffled())
-            }
-        }
-        
-        return selected.enumerated().map {
-            return GameItem(
-                type: withType,
-                question: $0.element,
-                answer: $0.element,
-                choices: choices[$0.offset]
-            )
-        }
-    }
-    
-    private func executeCategory2Style(
-        withType: GameType,
-        items: [ProductItem],
-        questionCount: Int,
-        choicesCount: Int
-    ) -> [GameItem] {
-        let selected: [ProductItem] = items.shuffled(count: questionCount)
-        var answers: [ProductItem] = []
-        var choices: [[ProductItem]] = []
-        
-        for index in 0..<questionCount {
-            let origin = selected[index]
-            let originFilter = items.filter { $0.id != origin.id }
-            
-            let equals = originFilter.filter { $0.itemCategory2 == origin.itemCategory2 }
-                .shuffled(count: choicesCount - 1)
-            let notEqual = originFilter.filter { $0.itemCategory2 != origin.itemCategory2 }
-                .shuffled()
-                .first
-            
-            guard let notEqual else {
-                answers.append(ProductItem(id: "", names: [], category: "", itemCategory2: ""))
-                choices.append(equals)
-                continue
-            }
-            
-            answers.append(notEqual)
-            choices.append((equals + [notEqual]).shuffled())
-        }
-        
-        return selected.enumerated().map {
-            return GameItem(
-                type: withType,
-                question: $0.element,
-                answer: answers[$0.offset],
-                choices: choices[$0.offset]
-            )
-        }
-    }
+
 }
 
 extension Array where Element == ProductItem {
